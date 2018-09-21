195a93106471a07358d28d32cac5643c29481a44
Brian Sadler <briansadler@MacBook-Pro-3.local>
2018-09-21 10:58:41 -0500

    Added the rest of the initial project files

---
+++ Guessing-Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guessing-Game
+//
+//  Created by Brian Sadler on 9/21/18.
+//  Copyright © 2018 Brian Sadler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
